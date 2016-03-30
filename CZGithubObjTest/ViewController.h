7a63f3fbe7463a6135a2d4e9194d6efd686cdced
xiaocuihero <xiaocuihero1@163.com>
2016-03-30 13:22:20 +0800

    Initial Commit

---
+++ CZGithubObjTest/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  CZGithubObjTest
+//
+//  Created by Cui on 16/3/30.
+//  Copyright © 2016年 Cui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+
+@end
+
---
+++ CZGithubObjTest/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  CZGithubObjTest
+//
+//  Created by Cui on 16/3/30.
+//  Copyright © 2016年 Cui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self ViewStruct];
+}
+
+- (void)ViewStruct {
+    
+}
+
+@end
